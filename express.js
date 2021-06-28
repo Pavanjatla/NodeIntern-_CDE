3d0a03cf9bc544d6a1748a98221d262cc5dc9f5a
Pavanjatla <53556145+Pavanjatla@users.noreply.github.com>
2021-06-28 16:31:04 +0530

    express

--- express.js
+++ express.js
@@ -2,9 +2,22 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.sendFile("./views/home.html", { root: __dirname });
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile('./views/about.html', { root: __dirname });
+});
+
+app.get('/about-me', (req, res) => {
+  res.redirect('about');
+});
+
+app.get('*', (req, res) => {
+  res.sendFile('./views/error.html', { root: __dirname });
+});
+
 
 app.listen(4000, () => {
   console.log(" port listening")
-})
+});
